01f579b7657ffa36253d7ad0ca319bd5d262969c
markov-avl <markov.avl@yandex.ru>
2024-11-25 00:15:52 +1000

    #28 Complete reflrection_utils tests

--- puripy/utils/reflection_utils.py
+++ puripy/utils/reflection_utils.py
@@ -2,10 +2,6 @@ import inspect
 from collections.abc import Callable
 
 
-def is_method_or_function(callable_: Callable) -> bool:
-    return inspect.ismethod(callable_) or inspect.isfunction(callable_)
-
-
 def is_defined_in_any(module_name: str, packages: set[str]) -> bool:
     return any(module_name.startswith(package) for package in packages)
 
